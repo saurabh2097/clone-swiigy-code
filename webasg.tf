c27ee2c68ee447a26b1f62aeda20ac0392b43ff3
saurabh2097 <saurabhchimankar97@gmail.com>
2025-04-01 12:24:01 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -16,7 +16,7 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-0ed194d7eff6d2f81"
   instance_type = "t2.micro"
-  key_name      = "devopsbyraham"
+  key_name      = "saurabhkeypair"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
